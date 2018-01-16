69af2acc3ef44e4c8dd1ec087273d8f38c7b56f6
Diego Galvão <diego.galvao@b2wdigital.com>
2018-01-16 00:16:16 -0300

    Add react native test

---
+++ button/src/Button.native.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Button } from 'react-native';
+
+class Button extends Component {
+  render() {
+    return (
+      <Button>teste</Button>
+    );
+  }
+}
