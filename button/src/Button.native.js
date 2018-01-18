b0930e248ba4290cd2cf0f27077c13b7c51eea4c
Diego Galvão <diego.galvao@b2wdigital.com>
2018-01-18 02:44:23 -0300

    add button native

--- button/src/Button.native.js
+++ button/src/Button.native.js
@@ -1,10 +1,59 @@
-import React, { Component } from 'react';
-import { Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button as RN_Button } from 'react-native';
 
-class Button extends Component {
-  render() {
-    return (
-      <Button>teste</Button>
-    );
-  }
+export default class Button extends React.Component  {
+    render() {
+
+        let {type, children, onClick, ...props} = this.props;
+
+        let buttonStyles = {
+            'primary': {
+                buttonContainer: {
+                    padding: 10,
+                    width: '100%',
+                    maxWidth: 400,
+                    overflow: 'hidden',
+                    borderRadius: 4,
+                    backgroundColor: '#40cd28',
+                    margin: 10,
+                    elevation:2,                    
+                },
+                button: {
+                    color:'#fff',
+                    fontSize:28,
+                    fontWeight: 'normal',
+                }
+            },
+            'default': {
+                buttonContainer: {
+                    paddingTop: 12,
+                    paddingBottom: 12,
+                    paddingRight: 36,
+                    paddingLeft: 36,
+                    width: 'auto',
+                    overflow: 'hidden',
+                    borderRadius: 4,
+                    backgroundColor: '#00adef',
+                    margin: 10,
+                    elevation:2,
+                },
+                button: {
+                    color:'#fff',
+                    fontSize:18,
+                    fontWeight: 'normal',
+                }
+            }
+        }
+
+        const styles = StyleSheet.create(buttonStyles[type]);
+
+        return (<RN_Button
+                    containerStyle={styles.buttonContainer}
+                    style={styles.button}
+                    accessibilityLabel={children}
+                    onPress={onClick}
+                    title={children}
+                    {...props}>
+                </RN_Button>);
+    }
 }
